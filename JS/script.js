99e53154d7a1ddf2e7af8180980707a0b0f3a4a7
Nikita <example@gmail.com>
2021-03-19 21:08:47 +0400

    Start project

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let result = prompt("Are you here?", "");
-console.log(result);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt("На сколько оцените его?", "");
+
+personaMovieDB.movies[a] = b;
+personaMovieDB.movies[c] = d;
+
+console.log(personaMovieDB);
